49336b31468f4fd178d25a46bae82563623a225b
venelouis <venelouistyago@gmail.com>
2025-01-12 17:52:33 -0300

    Add obstacle generation and game over logic to Ninja Runner

--- Games/Ninja Runner/ninja.py
+++ Games/Ninja Runner/ninja.py
@@ -13,9 +13,29 @@ runner.y = 400
 velocity_y = 0
 gravity = 1
 
+obstacles = []
+obstacles_timeout = 0
+
+score = 0
+game_over = False
+
 def update():
-  global velocity_y
+  global velocity_y, obstacles_timeout, score, game_over
   runner.next_image()
+
+  obstacles_timeout += 1
+  if obstacles_timeout > 50:
+    actor = Actor('cactus')
+    actor.x = 850
+    actor.y = 430
+    obstacles.append(actor)
+    obstacles_timeout = 0
+
+  for actor in obstacles:
+    actor.x -= 8
+    if actor.x < -50:
+      obstacles.remove(actor)
+      score += 1
 
   if keyboard.up:
     velocity_y = -15
@@ -26,9 +46,19 @@ def update():
     velocity_y = 0
     runner.y = 400
 
+  if runner.collidelist(obstacles) != -1:
+    game_over = True
+
 def draw():
   screen.draw.filled_rect(Rect(0,0,800,400), (163, 232, 254))
   screen.draw.filled_rect(Rect(0,400,800,200), (88, 242, 152))
-  runner.draw()
+  if game_over:
+    screen.draw.text('Game Over', centerx=400, centery=270, color=(255,255,255), fontsize=60)
+    screen.draw.text('Score: ' + str(score), centerx=400, centery=330, color=(255,255,255), fontsize=60)
+  else:
+    runner.draw()
+    for actor in obstacles:
+      actor.draw()
+    screen.draw.text('Score: ' + str(score), (15,10), color=(0,0,0), fontsize=30)
 
 pgzrun.go() # Must be last line
