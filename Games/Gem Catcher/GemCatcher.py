30a0048a3c1a127f3ed2a3f1bb73783da2e68739
venelouis <venelouistyago@gmail.com>
2025-01-12 15:16:10 -0300

    Add game over functionality and score-based gem speed increase

--- Games/Gem Catcher/GemCatcher.py
+++ Games/Gem Catcher/GemCatcher.py
@@ -13,19 +13,22 @@ gem.x = random.randint(20, 780)
 gem.y = 0
 
 score = 0
+game_over = False
+
+def on_mouse_move(pos, rel, buttons):
+    ship.x = pos[0]
 
 def update():
-    global score
+    global score, game_over
 
     if keyboard.left:
         ship.x = ship.x - 5
     if keyboard.right:
         ship.x = ship.x + 5
 
-    gem.y = gem.y + 4
+    gem.y = gem.y + 4 + score / 5
     if gem.y > 600:
-        gem.x = random.randint(20, 780)
-        gem.y = 0
+        game_over = True
     if gem.colliderect(ship):
         gem.x = random.randint(20, 780)
         gem.y = 0
@@ -33,8 +36,12 @@ def update():
 
 def draw():
     screen.fill((80,0,70))
-    gem.draw()
-    ship.draw()
-    screen.draw.text('Score: ' + str(score), (15,10), color=(255,255,255), fontsize=30)
+    if game_over:
+        screen.draw.text('Game Over', (360, 300), color=(255,255,255), fontsize=60)
+        screen.draw.text('Score: ' + str(score), (360, 350), color=(255,255,255), fontsize=60)
+    else:
+        gem.draw()
+        ship.draw()
+        screen.draw.text('Score: ' + str(score), (15,10), color=(255,255,255), fontsize=30)
 
 pgzrun.go() # Must be last line
